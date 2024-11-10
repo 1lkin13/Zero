e5d16fbcb1397cc67cbf0452ee9b6cd377396ac0
1lkin13 <ilkin.emiraslanov1453@gmail.com>
2024-11-11 03:00:22 +0400

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,52 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+const config: Config = {
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: "475px",
+            },
+            colors: {
+                primary: {
+                    "100": "#e3f2fd",
+                    DEFAULT: "#4072F4",
+                },
+                
+                secondary: "#FBE843",
+                black: {
+                    "100": "#333333",
+                    "200": "#141413",
+                    "300": "#7D8087",
+                    DEFAULT: "#201E1C",
+                },
+                white: {
+                    "100": "#F7F7F7",
+                    DEFAULT: "#FFFFFF",
+                },
+            },
+            fontFamily: {
+                "work-sans": ["var(--font-work-sans)"],
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            boxShadow: {
+                100: "2px 2px 0px 0px rgb(0, 0, 0)",
+                200: "2px 2px 0px 2px rgb(0, 0, 0)",
+                300: "2px 2px 0px 2px rgb(33, 118, 255)",
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
